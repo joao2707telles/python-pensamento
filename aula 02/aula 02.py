8a39b76c6c587b418111e0a627bf7efa1c8c4013
joao2707telles <joao2707telles@gmail.com>
2026-03-20 11:31:35 -0300

    aula 2 variaveis e operadores

---
+++ aula 02/aula 02.py
@@ -0,0 +1,31 @@
+from sphinx.cmd.build import jobs_argument
+
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENAÇÃO DE STRINGS
+# Comentário de 1 linha
+'''
+comentários de
+multiplas linhsa
+'''
+
+#VARIÁVEIS
+nome = ("Joao") # str
+idade = 18 # int
+peso = 80 #float
+
+print(nome, idade, peso)
+print(f"oiii {nome}!!!")
+print("Oiiii", nome, "!!!!")
+
+# INPUT - SIMULAÇÃO DE UM FORMS NO CMD
+nome = input("DIGITE O SEU NOME: ")
+idade = int(input("DIGITE SUA IDADE: "))
+
+print(nome, idade)
+print(idade +1)
+
+
+
---
+++ testea.py
@@ -0,0 +1,5 @@
+print("Seja bem-vindo(a)!")
+
+nome = input("DIGITE O SEU NOME: ")
+print(nome)
+print(f"Seja bem vindo(a) {nome}!!!")
